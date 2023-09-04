e71ed5390112a5caabee238f719f2b591d3e8196
rakdav <rakdav@inbox.ru>
2023-09-04 20:02:32 +0200

    lesson_5_2

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,9 +1,24 @@
-﻿double a = 8.31;
-float b = 4.6f;
-decimal c = 6.9m;
+﻿//double a = 8.31;
+//float b = 4.6f;
+//decimal c = 6.9m;
 
-int d = 6;
-byte e = 89;
-ulong f = 6668676867886787646L;
+//int d = 6;
+//byte e = 89;
+//ulong f = 9;
 
-const float pi = 3.14f;
+//const float pi = 3.14f;
+//2.25
+//Console.Write("Введите число x:");
+//int x = int.Parse(Console.ReadLine());
+//int c = x % 10;
+//int d = (x - c)/10;
+//int res = c * 100 + d;
+//Console.WriteLine(res);
+
+//2.24
+Console.WriteLine("Введите n:");
+int n=int.Parse(Console.ReadLine());
+int a = n / 100;
+int b = n % 100;
+int c = b * 10 + a;
+Console.WriteLine(c);
