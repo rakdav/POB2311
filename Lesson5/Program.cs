36e676e4ee1ebe451762a1f65d36796878c15565
rakdav <rakdav@inbox.ru>
2023-09-04 19:19:08 +0200

    lesson 5_1

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = 8.31;
+float b = 4.6f;
+decimal c = 6.9m;
+
+int d = 6;
+byte e = 89;
+ulong f = 6668676867886787646L;
+
+const float pi = 3.14f;
