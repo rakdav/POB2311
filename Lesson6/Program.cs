3eec5b531aad766bd1ced23eec5836f0a90de532
rakdav <rakdav@inbox.ru>
2023-09-04 21:14:48 +0200

    3.28

---
+++ Lesson6/Program.cs
@@ -0,0 +1,21 @@
+﻿//3.4
+//bool x = true;
+//bool y = true;
+//bool z = false;
+
+//Console.WriteLine(!x&&y);
+//Console.WriteLine(x||!y);
+//Console.WriteLine(x||y&&z);
+//3.11
+//a
+//int x = 1, y = -1;
+//Console.WriteLine(x*x+y*y<=4);
+//б
+//int x = 1, y = 2;
+//Console.WriteLine((x>=0)||(y*y!=4));
+//3.28
+Console.Write("Введите а:");
+int a=int.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine((a>100)&&(b>100));
