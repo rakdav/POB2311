ecf9a5432b270b735d4a3f0b4c4e7945dce67bc3
rakdav <rakdav@inbox.ru>
2023-09-06 19:50:59 +0200

    3.32

---
+++ Lesson7/Program.cs
@@ -0,0 +1,26 @@
+﻿//Проверить, принадлежит ли число введенное с клавиатуры, интервалу (-5; 3).
+//Console.Write("Введите x:");
+//double x=int.Parse(Console.ReadLine());
+//if ((x > -5) && (x < 3))
+//    Console.WriteLine("Принадлежит");
+//else
+//    Console.WriteLine("Не принадлежит");
+
+//Проверить, принадлежит ли число введенное с клавиатуры, интервалу (-5; 3).
+//Console.Write("Введите n:");
+//int n=int.Parse(Console.ReadLine());
+//int a = n % 10;
+//int b= n / 10;
+//if (a == 3 || b == 3) Console.WriteLine("Входит");
+//else Console.WriteLine("Не входит");
+
+//3.32a
+Console.Write("Введите x:");
+double x=double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+if (x <= -2 && y >= 1)
+    Console.WriteLine("Принадлежит");
+else
+    Console.WriteLine("Не принадлежит");
+
